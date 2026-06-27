e8354576c271343581c3aeafa4a31c3a70bc71e2
folltoshe <folltoshe@foxmail.com>
2026-06-27 22:54:20 +0800

    feat: add proto export

---
+++ src/proto.ts
@@ -0,0 +1,12 @@
+export * from '@gen/info_pb'
+
+export * from '@gen/common/time_pb'
+export * from '@gen/language/language_pb'
+export * from '@gen/agent/agent_pb'
+export * from '@gen/meta/meta_pb'
+
+export * from '@gen/word/content_pb'
+export * from '@gen/word/annotation_pb'
+
+export * from '@gen/line/content_pb'
+export * from '@gen/line/annotation_pb'
