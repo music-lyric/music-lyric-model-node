2c0e43521c28eb13fb6f2a94c6fc9bfd2e19405c
folltoshe <folltoshe@foxmail.com>
2026-07-07 17:35:54 +0800

    fix: preserve namespace export name in generated types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export * as Common from './common'
-export * as Runtime from './runtime'
-export * as Storage from './storage'
+import * as CommonModel from './common'
+import * as RuntimeModel from './runtime'
+import * as StorageModel from './storage'
+
+export { CommonModel as Common, RuntimeModel as Runtime, StorageModel as Storage }
 
 export * from './version'
